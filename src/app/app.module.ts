b2c40f3f6fe28d35093311d345ef76942c270c47
Maria <kanyalaekie@gmail.com>
2022-06-14 16:36:07 +0200

    feat: création de la navbar et du footer avec les liens effectifs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './commons/nav-bar/nav-bar.component';
-import { FooterComponent } from './commons/footer/footer.component';
-import { NotFoundComponent } from './commons/not-found/not-found.component';
+import { NavBarComponent } from './_commons/nav-bar/nav-bar.component';
+import { FooterComponent } from './_commons/footer/footer.component';
+import { NotFoundComponent } from './_commons/not-found/not-found.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ProductListComponent } from './pages/product-page/product-list/product-list.component';
+import { ProductCardComponent } from './pages/product-page/product-card/product-card.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { ProductPageComponent } from './pages/product-page/product-page.componen
     FooterComponent,
     NotFoundComponent,
     HomePageComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductListComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
